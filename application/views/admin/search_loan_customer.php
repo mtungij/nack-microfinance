cb3c4a4f08d04dd6b95629e5d2711083aa2214cc
mtungij <jamesmtungi2013@gmail.com>
2025-10-01 23:49:36 +0300

    hello

--- application/views/admin/search_loan_customer.php
+++ application/views/admin/search_loan_customer.php
@@ -517,12 +517,24 @@ include_once APPPATH . "views/partials/header.php";
 
 </div>
 
-<button type="submit" class="text-white inline-flex items-center bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800">
-    <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
+<button type="submit" 
+    class="inline-flex items-center 
+           text-black dark:text-white 
+           bg-red-500 hover:bg-red-600 
+           focus:ring-4 focus:outline-none focus:ring-red-300 
+           dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800
+           font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+    <svg class="mr-1 -ml-1 w-6 h-6" 
+         fill="currentColor" 
+         viewBox="0 0 20 20" 
+         xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" 
+              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" 
+              clip-rule="evenodd"></path>
     </svg>
     Samehe Faini
 </button>
+
 <?php echo form_close(); ?>
 
         </div>
