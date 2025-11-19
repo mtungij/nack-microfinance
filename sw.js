4e479f83c9af9b47ced119d5e80789b51ac40167
mtungij <jamesmtungi2013@gmail.com>
2025-11-19 14:53:04 +0300

    added

--- sw.js
+++ sw.js
@@ -1,11 +1,36 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker Installed");
+const CACHE_NAME = 'loan-pocket-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/manifest.json',
+  '/assets/images/icon.png',
+  '/assets/images/icon.png',
+  '/public/css/output.css',
+  // Add other assets you want cached here
+];
+
+self.addEventListener('install', event => {
+  console.log('Service Worker Installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('activate', event => {
+  console.log('Service Worker Activated');
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request);
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
