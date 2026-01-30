c7225f83d4e8ccd32a1e79ff7659c84df3f1f65f
mtungij <jamesmtungi2013@gmail.com>
2026-01-30 20:17:49 +0300

    report

--- application/views/officer/today_officer_transaction.php
+++ application/views/officer/today_officer_transaction.php
@@ -20,7 +20,7 @@ include_once APPPATH . "views/partials/officerheader.php";
     <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"></path>
     </svg>
-    Download PDF
+    Tuma Report
   </a>
 </div>
 
