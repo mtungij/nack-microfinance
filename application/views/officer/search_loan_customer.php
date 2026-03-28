23c692a668d9c293fcdb69c85c76aa9c6bdf7c8c
mtungij <jamesmtungi2013@gmail.com>
2026-03-28 16:47:10 +0300

    withdrow

--- application/views/officer/search_loan_customer.php
+++ application/views/officer/search_loan_customer.php
@@ -361,7 +361,7 @@ include_once APPPATH . "views/partials/officerheader.php";
         * Kiasi cha Kugawa:
       </label>
       <input type="text" id="withdrow_<?php echo $customer->customer_id; ?>" name="withdrow"
-  value="<?= htmlspecialchars(!empty($remain_balance->withdrow) ? $remain_balance->withdrow : 0, ENT_QUOTES, 'UTF-8'); ?>"
+ value="<?php echo $remain_balance->balance; ?>"
   class="py-2.5 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-cyan-500 focus:ring-cyan-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300 dark:placeholder-gray-500 dark:focus:ring-gray-600"
   required>
 
