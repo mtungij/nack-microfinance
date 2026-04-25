5e7d53885c3aad55e164b557cee77ffeee057625
mtungij <jamesmtungi2013@gmail.com>
2026-04-25 20:38:34 +0300

    Settings

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,8 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 	public function index(){
-		$this->load->model('queries');
-		$this->load->view('welcome');
+        return redirect('welcome/employee_login');
 	}
 
 public function create_company()
