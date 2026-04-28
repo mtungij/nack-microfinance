c2c22c9b83e5e35d64b1d64020caf0f4124c32a5
mtungij <jamesmtungi2013@gmail.com>
2026-04-28 20:44:48 +0300

    hesabu

--- application/views/partials/sideofficer.php
+++ application/views/partials/sideofficer.php
@@ -453,7 +453,7 @@ $communication_submenu_active = is_submenu_active(['send_email']); // Assuming S
                  
                   <!-- <li><a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm rounded-lg </?php echo is_active_link('admin/next_expectation') ? 'text-cyan-600 dark:text-cyan-500' : 'text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-gray-400 dark:hover:text-gray-300'; ?>" href="<?php echo base_url("admin/next_expectation"); ?>">Expected Receivable</a></li>  -->
 
-                  <li><a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm rounded-lg <?php echo is_active_link('oficer/mauzo_report') ? 'text-cyan-600 dark:text-cyan-500' : 'text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-gray-400 dark:hover:text-gray-300'; ?>" href="<?php echo base_url('oficer/mauzo_report'); ?>">Report ya Mauzo</a></li>
+                  <!-- <li><a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm rounded-lg <?php echo is_active_link('oficer/mauzo_report') ? 'text-cyan-600 dark:text-cyan-500' : 'text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-gray-400 dark:hover:text-gray-300'; ?>" href="<?php echo base_url('oficer/mauzo_report'); ?>">Report ya Mauzo</a></li> -->
 
                   </ul>
           </div>
